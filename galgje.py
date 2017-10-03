399240147f1710a45c2c5d6e8bd56cd2efca4d77
Pleun Emmelot <pleunelise.emmelot@coderclass.nl>
2017-10-03 10:27:00 +0200

    Vraag 1 + input toegevoegd

--- galgje.py
+++ galgje.py
@@ -1,9 +1,14 @@
-woorden = ['huis', 'dak', 'pot', 'zak']
+woord = ['zak']
 ggl = ['','']
 gl = ['','']
+fouten = 0
+print ("Welkom bij")
 print ("""
            __      __    __     __    ____  ____
           / _)    /__\  (  )   / _)  (_  _)(____)
          ( (_-.  /(__)\  )(__ ( (_-..-_)(   )__)
           \___/ (__)(__)(____) \___/\____) (____)""")
 
+vraag1 =  input(str("Kies een letter of kies een ? om het woord te raden.")
+if vraag1 > 1:
+    print("Vul aub 1 letter in of een ? om het woord te raden.")
