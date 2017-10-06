724e1eb549fe79602e2f217903166f987a46b7df
Pleun Emmelot <pleunelise.emmelot@coderclass.nl>
2017-10-06 08:45:37 +0200

    if letter goed en if letter fout toegevoegd

--- galgje.py
+++ galgje.py
@@ -1,6 +1,7 @@
-woord = ['zak']
-ggl = ['','']
-gl = ['','']
+woord = ["zak"]
+letters = ["z","a","k"]
+ggl = ["",""]
+gl = ["",""]
 fouten = 0
 print ("Welkom bij")
 print ("""
@@ -9,6 +10,13 @@ print ("""
          ( (_-.  /(__)\  )(__ ( (_-..-_)(   )__)
           \___/ (__)(__)(____) \___/\____) (____)""")
 
-vraag1 =  input(str("Kies een letter of kies een ? om het woord te raden.")
-if vraag1 > 1:
-    print("Vul aub 1 letter in of een ? om het woord te raden.")
+vraag1 =  input("Kies een letter of kies een ? om het woord te raden:")
+if vraag1 == letters:
+    print("Goedzo, je hebt een letter goed geraden")
+    gl + vraag1
+     
+print ("Dit zijn je goed geraden letters: " + ggl)
+print ("Dit zij alle geraden letters: " + gl)
+
+
+
