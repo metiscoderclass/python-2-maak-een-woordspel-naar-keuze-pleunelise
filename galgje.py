1a724549c8a3368c1b0f93a3e4914a0fddec5579
Pleun Emmelot <pleunelise.emmelot@coderclass.nl>
2017-10-06 09:36:15 +0200

    if letter goed/fout toevgen aan lijst fouten +1

--- galgje.py
+++ galgje.py
@@ -1,7 +1,7 @@
-woord = ["zak"]
-letters = ["z","a","k"]
-ggl = ["",""]
-gl = ["",""]
+woord = "zak"
+# letters = ["z","a","k"]
+ggl = []
+gl = []
 fouten = 0
 print ("Welkom bij")
 print ("""
@@ -10,13 +10,36 @@ print ("""
          ( (_-.  /(__)\  )(__ ( (_-..-_)(   )__)
           \___/ (__)(__)(____) \___/\____) (____)""")
 
-vraag1 =  input("Kies een letter of kies een ? om het woord te raden:")
-if vraag1 == letters:
-    print("Goedzo, je hebt een letter goed geraden")
-    gl + vraag1
-     
-print ("Dit zijn je goed geraden letters: " + ggl)
-print ("Dit zij alle geraden letters: " + gl)
+while True:
+    
+    vraag1 =  input("Kies een letter of kies een ? om het woord te raden:")
+    lengte = len(vraag1)
+    if lengte > 1:
+        input("Fout, probeer het nog een keer:")
+        fouten += 1
+    else:
+
+        if vraag1 in woord:
+            ggl.append(vraag1)
+            print("Goed geraden!")
+            print("Dit zij alle geraden letters: " + str(gl))
+            print("Dit zijn je goed geraden letters: " + str(ggl))
+
+        elif vraag1 == "?":
+            raad = input("Raad het woord:")
+
+        elif vraag1 not in woord:
+            gl.append(vraag1)
+            input("Fout, probeer het nog een keer:")
+            print("Dit zij alle geraden letters: " + str(gl))
+            print("Dit zijn je goed geraden letters: " + str(ggl))
+            fouten += 1
+
+        
+
+            if raad == woord:
+                print("Je hebt het woord goed geraden!")
+                break
 
 
 
