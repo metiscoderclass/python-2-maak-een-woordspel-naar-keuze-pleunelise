b814a5c95c754c632765143da3a9f55437e50ecf
Pleun Emmelot <pleunelise.emmelot@coderclass.nl>
2017-10-09 10:12:54 +0200

    if fouten+5 break & if alle letters gg raad het woord

--- galgje.py
+++ galgje.py
@@ -1,5 +1,4 @@
 woord = "zak"
-# letters = ["z","a","k"]
 ggl = []
 gl = []
 fouten = 0
@@ -14,6 +13,8 @@ while True:
     
     vraag1 =  input("Kies een letter of kies een ? om het woord te raden:")
     lengte = len(vraag1)
+    lengtewoord = len(woord)
+    lengteggl = len(ggl)
     if lengte > 1:
         input("Fout, probeer het nog een keer:")
         fouten += 1
@@ -30,16 +31,34 @@ while True:
 
         elif vraag1 not in woord:
             gl.append(vraag1)
-            input("Fout, probeer het nog een keer:")
             print("Dit zij alle geraden letters: " + str(gl))
             print("Dit zijn je goed geraden letters: " + str(ggl))
+            vraag2 = input("Fout, probeer het nog een keer:")
             fouten += 1
+            if vraag2 in woord:
+                ggl.append(vraag2)
+                print("Goed geraden!")
+                print("Dit zij alle geraden letters: " + str(gl))
+                print("Dit zijn je goed geraden letters: " + str(ggl))
 
-        
-
+            elif vraag2 not in woord:
+                gl.append(vraag2)
+                vraag2 = input("Fout, probeer het nog een keer:")
+                print("Dit zij alle geraden letters: " + str(gl))
+                print("Dit zijn je goed geraden letters: " + str(ggl))
+                fouten += 1
+            if fouten > 5:
+                einde = input("Je hebt 5 fouten kies kies stop om te stoppen:")
+                if einde == "stop":
+                    break     
+            
+        if lengteggl == lengtewoord:
+            raad = input("Raad het woord met alle goed geraden letters:")
             if raad == woord:
                 print("Je hebt het woord goed geraden!")
                 break
+            else:
+               input("Fout, probeer het nog een keer:")
 
 
 
