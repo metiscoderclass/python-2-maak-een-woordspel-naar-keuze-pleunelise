acb3442f3f268c7822bc77a1095d05c8de1761d6
Pleun Emmelot <pleunelise.emmelot@coderclass.nl>
2017-10-12 09:48:47 +0200

    if letter al in gl lijst remove

--- galgje.py
+++ galgje.py
@@ -10,83 +10,118 @@ print ("""
           \___/ (__)(__)(____) \___/\____) (____)""")
 
 while True:
-    
+
     vraag1 =  input("Kies een letter of kies een ? om het woord te raden:")
     lengte = len(vraag1)
     lengtewoord = len(woord)
     lengteggl = len(ggl)
+
+
+
+    if lengteggl == lengtewoord:
+        raad = input("Raad het woord met alle goed geraden letters:")
+        if raad == woord:
+            print("Je hebt het woord goed geraden!")
+
+        else:
+            input("Fout, probeer het nog een keer:")
+
+
+
+
     if lengte > 1:
-        input("Fout, probeer het nog een keer:")
         fouten += 1
+        print("Druk op enter")
+        input("Fout, probeer het nog een keer:")
+
     else:
 
-        if vraag1 in woord:
+        if vraag1 in gl:
+            print("Je hebt deze letter al geraden al geraden probeer nog een keer.")
+
+
+        elif vraag1 in woord:
             ggl.append(vraag1)
             print("Goed geraden!")
             print("Dit zij alle geraden letters: " + str(gl))
             print("Dit zijn je goed geraden letters: " + str(ggl))
-            if vraag1 in ggl:
-                gl.remove(vraag1)
-                ggl.remove(vraag1)
-                input("Je hebt dit woord al geraden, probeer het opnieuw:")
 
         elif vraag1 == "?":
             raad = input("Raad het woord:")
 
         elif vraag1 not in woord:
             gl.append(vraag1)
+            fouten += 1
+            print("Druk op enter")
+            vraag2 = input("Fout, probeer het nog een keer:")
             print("Dit zij alle geraden letters: " + str(gl))
             print("Dit zijn je goed geraden letters: " + str(ggl))
-            vraag2 = input("Fout, probeer het nog een keer:")
-            fouten += 1
-            if fouten == 1:
-                print("""
-                          |
-                          |
-                          |
-                          |
-                          |
-                          | """)
-            elif fouten == 2:
-                print("""
-                          __________
-                         |
-                         |
-                         |
-                         |
-                         |
-                         |
-                         | """)
-            elif fouten == 3:
-                print("""
-                          __________
-                         |/
-                         |
-                         |
-                         |
-                         |
-                         |
-                         | """)
-            elif fouten == 4:
-                print("""
-                          __________
-                         |/   |
-                         |    
-                         |
-                         |
-                         |
-                         |
-                         | """)
-            elif fouten == 5:
-                print("""
-                          __________
-                         |/   |
-                         |   ( )
-                         |   \|/
-                         |    |
-                         |   / \
-                         |
-                         | """)
+            if vraag1 in gl:
+                gl.remove(vraag1)
+            print("Druk op enter")
+
+
+
+        if fouten == 1:
+            print("""
+                    |
+                    |
+                    |
+                    |
+                    |
+                    |""")
+        elif fouten == 2:
+            print("""
+                    ________
+                    |
+                    |
+                    |
+                    |
+                    |
+                    |
+                    """)
+        elif fouten == 3:
+            print("""
+
+                ________
+                |/
+                |
+                |
+                |
+                |
+                |
+
+
+
+
+                """ )
+
+        elif fouten == 4:
+            print("""
+
+                ________
+                |/  |
+                |
+                |
+                |
+                |
+                |
+                """)
+
+        elif fouten == 5:
+
+            print("""
+
+                ________
+                |/  |
+                |  ( )
+                |  \|/
+                |  / \
+                |
+                |
+                """)
+
+
             if vraag2 in woord:
                 ggl.append(vraag2)
                 print("Goed geraden!")
@@ -99,18 +134,8 @@ while True:
                 print("Dit zij alle geraden letters: " + str(gl))
                 print("Dit zijn je goed geraden letters: " + str(ggl))
                 fouten += 1
+
             if fouten > 5:
-                einde = input("Je hebt 5 fouten kies kies stop om te stoppen:")
+                einde =input("Je hebt 5 fouten kies stop om te stoppen.")
                 if einde == "stop":
-                    break     
-            
-        if lengteggl == lengtewoord:
-            raad = input("Raad het woord met alle goed geraden letters:")
-            if raad == woord:
-                print("Je hebt het woord goed geraden!")
-                break
-            else:
-               input("Fout, probeer het nog een keer:")
-
-
-
+                    break
