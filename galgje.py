764794186997958348537a344bfd1f5d928c560b
Pleun Emmelot <pleunelise.emmelot@coderclass.nl>
2017-10-12 10:30:21 +0200

    galgje1.0 alleen nog streepjes toevoegen

--- galgje.py
+++ galgje.py
@@ -10,41 +10,25 @@ print ("""
           \___/ (__)(__)(____) \___/\____) (____)""")
 
 while True:
-
+    
     vraag1 =  input("Kies een letter of kies een ? om het woord te raden:")
     lengte = len(vraag1)
     lengtewoord = len(woord)
     lengteggl = len(ggl)
-
-
-
-    if lengteggl == lengtewoord:
-        raad = input("Raad het woord met alle goed geraden letters:")
-        if raad == woord:
-            print("Je hebt het woord goed geraden!")
-
-        else:
-            input("Fout, probeer het nog een keer:")
-
-
-
-
     if lengte > 1:
-        fouten += 1
-        print("Druk op enter")
         input("Fout, probeer het nog een keer:")
-
+        fouten += 1
     else:
 
-        if vraag1 in gl:
-            print("Je hebt deze letter al geraden al geraden probeer nog een keer.")
-
-
-        elif vraag1 in woord:
+        if vraag1 in woord:
             ggl.append(vraag1)
             print("Goed geraden!")
             print("Dit zij alle geraden letters: " + str(gl))
             print("Dit zijn je goed geraden letters: " + str(ggl))
+            if vraag1 in ggl:
+                gl.remove(vraag1)
+                ggl.remove(vraag1)
+                input("Je hebt dit woord al geraden, probeer het opnieuw:")
 
         elif vraag1 == "?":
             raad = input("Raad het woord:")
@@ -59,69 +43,54 @@ while True:
             if vraag1 in gl:
                 gl.remove(vraag1)
             print("Druk op enter")
-
-
-
-        if fouten == 1:
-            print("""
-                    |
-                    |
-                    |
-                    |
-                    |
-                    |""")
-        elif fouten == 2:
-            print("""
-                    ________
-                    |
-                    |
-                    |
-                    |
-                    |
-                    |
-                    """)
-        elif fouten == 3:
-            print("""
-
-                ________
-                |/
-                |
-                |
-                |
-                |
-                |
-
-
-
-
-                """ )
-
-        elif fouten == 4:
-            print("""
-
-                ________
-                |/  |
-                |
-                |
-                |
-                |
-                |
-                """)
-
-        elif fouten == 5:
-
-            print("""
-
-                ________
-                |/  |
-                |  ( )
-                |  \|/
-                |  / \
-                |
-                |
-                """)
-
-
+            if fouten == 1:
+                print("""
+                          |
+                          |
+                          |
+                          |
+                          |
+                          | """)
+            elif fouten == 2:
+                print("""
+                          __________
+                         |
+                         |
+                         |
+                         |
+                         |
+                         |
+                         | """)
+            elif fouten == 3:
+                print("""
+                          __________
+                         |/
+                         |
+                         |
+                         |
+                         |
+                         |
+                         | """)
+            elif fouten == 4:
+                print("""
+                          __________
+                         |/   |
+                         |    
+                         |
+                         |
+                         |
+                         |
+                         | """)
+            elif fouten == 5:
+                print("""
+                          __________
+                         |/   |
+                         |   ( )
+                         |   \|/
+                         |    |
+                         |   / \
+                         |
+                         | """)
             if vraag2 in woord:
                 ggl.append(vraag2)
                 print("Goed geraden!")
@@ -130,12 +99,30 @@ while True:
 
             elif vraag2 not in woord:
                 gl.append(vraag2)
+                print("Druk op enter")
                 vraag2 = input("Fout, probeer het nog een keer:")
                 print("Dit zij alle geraden letters: " + str(gl))
                 print("Dit zijn je goed geraden letters: " + str(ggl))
                 fouten += 1
-
+                if vraag2 in gl:
+                    gl.remove(vraag2)
+                print("Druk op enter")
+                
             if fouten > 5:
-                einde =input("Je hebt 5 fouten kies stop om te stoppen.")
+                einde = input("Je hebt 5 fouten kies kies stop om te stoppen:")
                 if einde == "stop":
-                    break
+                    break     
+            
+        if lengteggl == lengtewoord:
+            for l in woord:
+                kwoord = kwoord.replace(l,"")
+                print(kwoord)
+            raad = input("Raad het woord met alle goed geraden letters:")
+            if raad == woord:
+                print("Je hebt het woord goed geraden!")
+                break
+            else:
+               input("Fout, probeer het nog een keer:")
+
+
+
